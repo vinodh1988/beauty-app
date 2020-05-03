53b7b27a184f84ffa88529ab24addaf6abe09643
vinodh1988 <k.c.vinodh@hotmail.com>
2020-05-03 16:57:10 +0530

    First  commit

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import  '../node_modules/bootstrap/dist/js/bootstrap.min';
+import  '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
 
 ReactDOM.render(
   <React.StrictMode>
